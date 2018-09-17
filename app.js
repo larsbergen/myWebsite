06f358aeafbc51ed60ac16b88226a632cea9485f
LBergen <lars.derek.bergen@gmail.com>
2018-09-17 02:59:27 -0700

    fixed

--- app.js
+++
@@ -1,6 +0,0 @@
-// the js will be comming soon
-//  my portfolio will have clickable images with 
-// info that will send you to a project that ive 
-// done, and the contact page will email me 
-// whatever you type into the text feild
-// also a complete mobile responsive navbar menu
